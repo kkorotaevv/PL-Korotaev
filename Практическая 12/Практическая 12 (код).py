4609f5a5a25f9a3e26451ec669dbb7131116d061
kkorotaevv <kkorotaevv@mail.ru>
2022-11-30 16:25:37 +0300

    12

--- Практическая 12/Практическая 12 (код).py
+++ Практическая 12/Практическая 12 (код).py
@@ -13,7 +13,7 @@ print(fuct(x, n))
 print('БЛОК Б')
 print('ЗАДАНИЕ 1')
 def str():
-    a = int(input())
+    a = int(input('Введите число '))
     if a == 0:
         return 1
     return max(a, str())
